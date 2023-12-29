027d2821d9082a1924b38a02c7e0fa63df5d8ff5
steve <brinkley0413@gmail.com>
2023-12-29 13:04:36 -0800

    icon change

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewChild, ElementRef, AfterViewInit  } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+
+})
+export class HomeComponent {
+
+}
